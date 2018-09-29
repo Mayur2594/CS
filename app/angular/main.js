e1a589e9d4207084f102fbe997e2878b5645cc8f
Mayur Mhatre <mhatremayur2520@gmail.com>
2018-09-29 18:29:06 +0530

    forgot password

--- app/angular/main.js
+++ app/angular/main.js
@@ -93,4 +93,22 @@ angular.module('CSApp', ['ngSanitize','ngRoute','ngResource','ui.bootstrap','ngF
        if(input!=undefined)
         {return input.slice(start);}
     }
-});
+}).directive('inputFocusFunction', function() {
+    'use strict';
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        // Parse the attribute to accomodate assignment to an object
+        var parseObj = attr.inputFocusFunction.split('.');
+        var attachTo = scope;
+        for (var i = 0; i < parseObj.length - 1; i++) {
+          attachTo = attachTo[parseObj[i]];
+        }
+        // assign it to a function that focuses on the decorated element
+        attachTo[parseObj[parseObj.length - 1]] = function() {
+          element[0].focus();
+        };
+      }
+    };
+  });
+
--- app/scripts/Controllers/LoginController.js
+++ app/scripts/Controllers/LoginController.js
@@ -1,11 +1,30 @@
 angular.module('CSApp')
-.controller('LoginController',function ($scope,$http,$route,$location) {
+.controller('LoginController',function ($scope,$http,$route,$location,$window,$timeout) {
 	
 	
+  $timeout( function(){
+            $scope.test1 = "Hello World!";
+        }, 5000 );
+
+        //time
+        $scope.time = 60;
+        
+        //timer callback
+        var timer = function() {
+            if( $scope.time > 0 ) {
+                $scope.time -= 1;
+                $timeout(timer, 1000);
+            }
+			
+        }
+        
+        //run!!
+       
 	
-	$scope.showHidePassword = function()
+	
+	$scope.showHidePassword = function(elemid)
   {
-		var passwordfield = document.getElementById("password");
+		var passwordfield = document.getElementById(elemid);
 		if (passwordfield.type === "password") {
 			passwordfield.type = "text";
 		}
@@ -17,11 +36,10 @@ angular.module('CSApp')
 		
   };
   
-	$scope.forgotpassword = function(degree)
+	$scope.forgotpasswordWindow = function(degree)
 	{
 			var elm = document.getElementById("psfld");
 			elm.classList.toggle("hiddenelem");
-			console.log(elm.classList)
 			document.getElementById("flippanel").style.transform = "rotateY("+degree+"deg)";
 	};
 	
@@ -43,8 +61,106 @@ angular.module('CSApp')
 			}
 		});
 	};
+	
+	
+	$scope.ResetPassword = function()
+	{			
+			$http({
+			method  : 'POST',
+			url     : '/api/ResetPassword',
+			data    : $scope.usersPassworddata ,
+			headers : {'Content-Type': 'application/json'} 
+		}).then(function(response) {
+			alert(response.data.message);
+			location.reload();
+		});
+	};
+	
+	$scope.arrayObj = [{otp:''},{otp:''},{otp:''},{otp:''},{otp:''},{otp:''}];
+	$scope.focusIndex = 0;
+	$scope.ForgotPassword = function()
+	{		
+
+		 $http({
+			method  : 'POST',
+			url     : '/api/ForgotPassword',
+			data    : $scope.user ,
+			headers : {'Content-Type': 'application/json'} 
+		}).then(function(response) {
+			
+			if(response.data.success === true)
+			{
+				$scope.message =  response.data.message;
+				$('#myModallOTP').modal({
+                        backdrop: 'static',
+                        keyboard: true, 
+                        show: true
+                });
+				 $timeout( function(){
+					 $scope.showbtn = true;
+							$timeout(timer, 1000); 
+					}, 20000 );
+				 
+	
+			}
+			else
+			{
+				alert(response.data.message)
+			}
+		}); 
+	};
+
+		$scope.SetFocus = function(index)
+		{
+			$scope.focusIndex = index;
+		};
+		
+		$scope.SubmitOtpAnResetpassword = function()
+		{
+			var OTP = '';
+			$scope.arrayObj.map(function(indval)
+			{
+				OTP = OTP+''+indval.otp;
+			});
+			
+			$http({
+              method: 'GET'
+              , url: '/api/verifyOTP/'+OTP.trim()
+              , dataType: 'jsonp'
+			}).then(function (response) {
+				if(response.data.status === 0)
+				{
+					$('#myModallOTP').modal({show: false});
+					$('#myModallNewPassword').modal({
+                        backdrop: 'static',
+                        keyboard: true, 
+                        show: true
+                });
+				}
+				else
+				{
+					alert(response.data.message);
+				}
+		});
+		};
 		
 	
 	
-});
+}).directive('customAutofocus', function() {
+  return{
+         restrict: 'A',
+
+         link: function(scope, element, attrs){
+           scope.$watch(function(){
+             return scope.$eval(attrs.customAutofocus);
+             },function (newValue){
+               if (newValue == true){
+                   element[0].focus();
+               }
+           });
+         }
+     };
+})
+;
+
 	
